16485041076b78582b2f3518d40bd9af7005e1b5
Joe Lauer <joe@lauer.bz>
2022-12-19 22:09:30 -0500

    More macos

---
+++ setup/build-tokyocabinet-macos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.48.tar.gz
+tar zxvf tokyocabinet-1.4.48.tar.gz
+cd tokyocabinet-1.4.48
+./configure
+make -j4
+make install
+
+wget http://fallabs.com/tokyocabinet/javapkg/tokyocabinet-java-1.24.tar.gz
+tar zxvf tokyocabinet-java-1.24.tar.gz
+cd tokyocabinet-java-1.24
+./configure
+make -j4
+make install
+
+#mkdir -p /target/libs
+#cp /usr/lib/libtokyocabinet.so /target/libs/
+#cp /usr/lib/libjtokyocabinet.so /target/libs/
+#chown 777 /target/libs/*
+#ls -la /target/libs
